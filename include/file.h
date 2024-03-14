7a833cd74f66daceb043c1512a58eb0eb547e9f3
Eltryo <david.merkl.dm@gmail.com>
2024-03-14 18:06:49 +0100

    Refactoring

---
+++ include/file.h
@@ -0,0 +1,8 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdio.h>
+char *readFile(FILE *fptr, size_t *bufferLength);
+void getInputFile(FILE **fptr, char *execPath, char *inputFilePath);
+
+#endif /* string.h */
--- src/day03/test.c
+++ src/day03/test.c
@@ -1,7 +1,22 @@
 #include <assert.h>
+#include <libgen.h>
+#include <linux/limits.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "../../include/partOne.h"
-#include "../../include/partOne.h"
+#include "../../include/file.h"
 
-int main(){
+void testPartOne(char *execPath, char *inputFilePath, int expected){
+    FILE *fptr;
+
+    getInputFile(&fptr, execPath, inputFilePath);
+
+    int result = partOne(fptr);
+
+    assert(result == 4361);
+}
+
+int main(int argc, char **argv){
+    testPartOne(argv[0], "day03/example_input_1", 4361);
 }
