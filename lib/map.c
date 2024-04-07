f23d68b8cb81ebd58782bdbedd994b9ab825750c
Eltryo <david.merkl.dm@gmail.com>
2024-04-08 00:07:08 +0200

    Add get method and partially add method

--- lib/map.c
+++ lib/map.c
@@ -29,6 +29,34 @@ Map *mapInit(){
     return map;
 }
 
+//todo: complete and test method
+void add(Map *map, int key, int value){
+    for (int i = 0; i < size; i++) {
+    }
+}
+
+//todo: test method
+Llist *get(Map *map, int key){
+    if(!(map)) {
+        fprintf(stderr, "map must not be NULL\n");
+        return NULL;
+    }
+
+    if(!(map->head)) {
+        fprintf(stderr, "map is empty\n");
+        return NULL;
+    }
+
+    MapNode *curr = map->head;
+    for(int i = 0; i < size; i++){
+        if (curr->key == key) {
+            return curr->value;
+        }
+    }
+
+    return NULL;
+}
+
 void freeMapNodes(MapNode *head){
     if(!head) return;
     if(head->next){
